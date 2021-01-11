12fcd3c91ea643b98e304d21d00ae39865b8d6a5
Eklas Abdi <eklasabdi@gmail.com>
2021-01-11 20:39:09 +0000

    changed assert test to chai test

--- test/tail_test.js
+++ test/tail_test.js
@@ -1,5 +1,9 @@
+const assert = require('chai').assert;
+// 
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
